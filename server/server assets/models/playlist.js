ce0538e7b46f3ce078cbe6190614356a84ee1437
michael <michael.green94@hotmail.com>
2018-09-02 14:22:45 -0600

    server changes

--- server/server assets/models/playlist.js
+++ server/server assets/models/playlist.js
@@ -4,11 +4,18 @@ let schemaName = 'Playlist'
 
 let songSchema = new Schema({
   //what to keep on a song
-  name: { type: String }
+  trackName: { type: String, required: true },
+  albumArt: { type: String, required: true },
+  artistName: { type: String, required: true },
+  collection: { type: String, required: true },
+  previewUrl: { type: String, required: true }
+
 })
 
 let schema = new Schema({
+  ownerName: { type: String, required: true },
   title: { type: String, required: true },
+  vote: { type: Number, default: 0 },
   songs: [songSchema]
 })
 
